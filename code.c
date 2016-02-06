bb4d15676154f5c8b5763a2cca92d441e71be0cd
Seed <seed@ubuntu.(none)>
2016-02-05 20:25:18 -0800

    first commit

---
+++ code.c
@@ -0,0 +1,45 @@
+#include <openssl/evp.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	int read;
+	printf("hi");
+	fp = fopen("dictionary.txt", "r");
+	if (fp == NULL) printf("failure");
+	while ((read = getline(&line, &len, fp)) != -1) {
+		unsigned char* key[16];
+		unsigned char* output[16];
+		char intext = "This is a top secret."
+		size_t i;
+		for (i = 0; i < read; ++i) {
+			key[i] = &line[i];
+		}
+		while (read < 16) {
+			char blah = ' ';
+			key[read] = &blah;
+			++read;
+		}
+		for (i = 0; i < 16; ++i) {
+			printf("%c\n", *key[i]);
+		}
+		printf("%s", *key);
+		EVP_CIPHER_CTX *ctx;
+		unsigned char iv[] = "0000000000000000";
+		EVP_CipherInit_ex(&ctx, EVP_aes_128_cbc(), NULL, NULL, NULL, do_encrypt);
+		OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == 16);
+		OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == 16);
+		EVP_CipherInit_ex(ctx, NULL, NULL, key, iv, do_encrypt);
+	}
+	fclose(fp);
+	//EVP_CIPHER_CTX *ctx;
+	//unsigned char iv[] = "0000000000000000";
+	//EVP_CipherInit_ex(&ctx, EVP_aes_128_cbc(), NULL, NULL, NULL, do_encrypt);
+	//OPENSSL_assert(EVP_CIPHER_CTX_key_length(ctx) == 16);
+	//OPENSSL_assert(EVP_CIPHER_CTX_iv_length(ctx) == 16);
+	//EVP_CipherInit_ex(ctx, NULL, NULL, key, iv, do_encrypt);
+	//return 0;
+}
